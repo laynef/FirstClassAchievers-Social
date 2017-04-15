5ac83b07acd980f37fdb600a85eacd3f56070386
laynefaler <laynefaler@gmail.com>
2017-04-14 22:10:37 -0700

    associations

--- database/models/profile.js
+++ database/models/profile.js
@@ -15,6 +15,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        Profile.belongsTo(models.User)
       }
     }
   });
--- database/models/user.js
+++ database/models/user.js
@@ -7,6 +7,8 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        User.hasMany(models.Testimonial)
+        User.hasOne(models.Profile)
       }
     }
   });
