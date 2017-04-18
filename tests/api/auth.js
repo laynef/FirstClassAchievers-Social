34f78aa6487daaf272a43bf01f35b4cf24272cc9
laynefaler <laynefaler@gmail.com>
2017-04-18 14:26:18 -0700

    about to test ajax calls

---
+++ tests/api/auth.js
@@ -0,0 +1,66 @@
+const assert = require('assert')
+const axios = require('axios')
+
+// Login
+describe('Login', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+})
+
+// Register
+describe('Register', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+})
+
+// Change Password
+describe('Change Password', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4))
+    })
+  })
+})
+
+// Logout
+// describe('Logout', function() {
+//   describe('Logout Success', function() {
+//     let result = null
+//     axios.get(`http://localhost:3232/auth/local/logout`)
+//       .then(response => {
+//         result = true
+//       })
+//       .catch(err => {
+//         result = false
+//       })
+//     while(result == null) {}
+//     it('should return true', function() {
+//       assert.deepEqual(result).to.be.true
+//     })
+//   })
+// })
+
