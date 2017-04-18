03a1a5fee62f4fca60eb1553545afa67af645cfa
laynefaler <laynefaler@gmail.com>
2017-04-18 06:27:16 -0700

    migrations

--- database/models/user.js
+++ database/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    email: DataTypes.STRING(250),
+    email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
     classMethods: {
