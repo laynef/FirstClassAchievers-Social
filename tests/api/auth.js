600aa92f9c6643e0630c3b656513ed98fbd2f6a4
laynefaler <laynefaler@gmail.com>
2017-04-18 18:47:59 -0700

    testing backend

--- tests/api/auth.js
+++ tests/api/auth.js
@@ -1,66 +1,114 @@
 const assert = require('assert')
 const axios = require('axios')
+const _ = require('lodash')
+
+let count = 0
+// Auth
+describe('Auth', function() {
+  count++ // constantly changing
 
 // Login
-describe('Login', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
+  describe('Login', function() {
+    it('Login Successful', function(done) {
+      let data= {email: 'admin@email.com', password: 'pass1234'}
+      axios.post(`http://localhost:3232/auth/local/login`, data)
+        .then(response => {
+          assert.equal(true).to.be.true
+        })
+        .catch(err => {
+          assert.equal(false).to.be.true
+        })
+        done()
+    })
+
+    it('Fail Wrong password', function(done) {
+      let data= {email: 'wrong@emasdfs.@#(*DF', password: 'pass1234'}
+      axios.post(`http://localhost:3232/auth/local/login`, data)
+        .then(response => {
+          assert.equal(false).to.be.true
+        })
+        .catch(err => {
+          assert.equal(true).to.be.true
+        })
+        done()
+    })
+
+    it('Fail Wrong password', function(done) {
+      let data= {email: 'admin@email.com', password: 'wrong'}
+      axios.post(`http://localhost:3232/auth/local/login`, data)
+        .then(response => {
+          assert.equal(false).to.be.true
+        })
+        .catch(err => {
+          assert.equal(true).to.be.true
+        })
+        done()
     })
   })
 
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
-    })
-  })
-})
 
 // Register
-describe('Register', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
+  describe('Register', function() {
+    it('Register Successful', function(done) {
+      let data= {email: `email${count}@email.com`, password: 'pass1234'}
+      axios.post(`http://localhost:3232/auth/local/register`, data)
+        .then(response => {
+          assert.equal(true).to.be.true
+        })
+        .catch(err => {
+          assert.equal(false).to.be.true
+        })
+        done()
     })
-  })
 
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
+    it('No email', function(done) {
+      let data ={email: '', password:'pass1234'}
+      axios.post(`http://localhost:3232/auth/local/register`, data)
+        .then(response => {
+          assert.equal(false).to.be.true
+        })
+        .catch(err => {
+          assert.equal(true).to.be.true
+        })
+        done()
+    })   
+    
+    it('No password', function(done) {
+      let data ={email: '', password:'pass1234'}
+      axios.post(`http://localhost:3232/auth/local/register`, data)
+        .then(response => {
+          assert.equal(false).to.be.true
+        })
+        .catch(err => {
+          assert.equal(true).to.be.true
+        })
+        done()
     })
   })
-})
 
 // Change Password
-describe('Change Password', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
-    })
-  })
+  // describe('#indexOf()', function() {
+  //   it('should return -1 when the value is not present', function(done) {
+  //     assert.equal(-1, [1,2,3].indexOf(4))
+  //   })
 
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4))
+  //   it('should return -1 when the value is not present', function(done) {
+  //     assert.equal(-1, [1,2,3].indexOf(4))
+  //   })
+  // })
+
+// Logout
+  describe('Logout Success', function() {
+    it('should return true', function(done) {
+      axios.get(`http://localhost:3232/auth/local/logout`)
+        .then(response => {
+          assert.equal(true).to.be.true
+        })
+        .catch(err => {
+          assert.equal(false).to.be.true
+        })
+        done()
     })
   })
 })
-
-// Logout
-// describe('Logout', function() {
-//   describe('Logout Success', function() {
-//     let result = null
-//     axios.get(`http://localhost:3232/auth/local/logout`)
-//       .then(response => {
-//         result = true
-//       })
-//       .catch(err => {
-//         result = false
-//       })
-//     while(result == null) {}
-//     it('should return true', function() {
-//       assert.deepEqual(result).to.be.true
-//     })
-//   })
-// })
 
