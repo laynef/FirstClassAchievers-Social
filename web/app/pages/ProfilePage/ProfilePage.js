ec2ef07bae9c48b5c92867c1d836314f490c0996
laynefaler <laynefaler@gmail.com>
2017-04-19 16:54:07 -0700

    dropzone

--- web/app/pages/ProfilePage/ProfilePage.js
+++ web/app/pages/ProfilePage/ProfilePage.js
@@ -2,22 +2,19 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import PrimaryContact from '../../components/Profile/PrimaryContact'
+import ProfileImageModal  from '../../components/Profile/ProfileImageModal'
 
 
 class ProfilePage extends Component {
-
-    constructor(props, context) {
-        super(props, context)
-        this.state = {
-
-        }
-    }
 
     render() {
         return (
             <div id="ProfilePage" className="col-md-12">
                 <h1>My Profile</h1>
-                <span id="profileImage" className="thumbnail-wrapper d32 circular inline m-t-5">
+                <span id="profileImage"
+                    className="thumbnail-wrapper d32 circular inline m-t-5"
+                    data-toggle="modal"
+                    data-target="#profile-image-modal">
                     <img src="theme/assets/img/profiles/fine.jpg" 
                         alt="" 
                         data-src="theme/assets/img/profiles/fine.jpg" 
@@ -26,6 +23,7 @@ class ProfilePage extends Component {
                         height="320"/>
                 </span>
                 <PrimaryContact />
+                <ProfileImageModal />
             </div>
         )
     }
