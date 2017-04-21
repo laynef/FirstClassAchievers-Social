cd4fbf1509ce8e149c6de17f4d445cc023b602b7
laynefaler <laynefaler@gmail.com>
2017-04-20 18:39:36 -0700

    fixed merge

--- web/app/Root.js
+++ web/app/Root.js
@@ -5,21 +5,21 @@ import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-// import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk'
 // import { createLogger } from 'redux-logger'
-// import ReduxPromise from 'redux-promise'
+import ReduxPromise from 'redux-promise'
 import reducers from './redux/store/combineReducers'
 import routes from './Router'
 
 
 // const logger = createLogger()
 const store = createStore(
-    reducers
-    // applyMiddleware(
-    //     reduxThunk, 
-    //     ReduxPromise, 
+    reducers,
+    applyMiddleware(
+        reduxThunk, 
+        ReduxPromise
     //     logger
-    // )
+    )
 )
 
 ReactDOM.render(
