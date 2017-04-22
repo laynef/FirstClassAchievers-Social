d264af1bbaaceed0b1966b5137d222c20dac05e5
laynefaler <laynefaler@gmail.com>
2017-04-22 14:05:13 -0700

    editted validations

--- web/app/redux/validators/profile.js
+++ web/app/redux/validators/profile.js
@@ -2,11 +2,5 @@ export function validate(values) {
 	const errors = {};
 	if (!values.firstName) errors.firstName = 'Enter your first name';
 	if (!values.lastName) errors.lastName = 'Enter your last name';
-	if (!values.city) errors.city = 'Enter your city';
-	if (!values.state) errors.state = 'Enter your state';
-	if (!values.country) errors.country = 'Enter your country';
-	if (!values.zipCode) errors.zipCode = 'Enter your zip code';
-	if (!values.goals) errors.goals = 'What are your goals?';
-	if (!values.position) errors.position = 'Enter your current position';
 	return errors;
 }
