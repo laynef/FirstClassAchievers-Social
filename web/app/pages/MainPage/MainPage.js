96a43c42815fee41b9d973ef4be56269d5962689
laynefaler <laynefaler@gmail.com>
2017-04-25 18:42:24 -0700

    news feed bug fixed

--- web/app/pages/MainPage/MainPage.js
+++ web/app/pages/MainPage/MainPage.js
@@ -49,7 +49,11 @@ class MainPage extends Component {
                                         author={entry.author}
                                         message={entry.message}
                                         image={entry.image}
-                                        userId={entry.user_id}
+                                        profileId={entry.user_id}
+                                        entryId={entry.id}
+                                        userId={user ? user.id : null}
+                                        favorites={favorites ? favorites.entries : null}
+                                        detail={true}
                                     />
                                 ))}
                             </div>
@@ -68,5 +72,6 @@ MainPage = reduxForm({
 export default connect(state => ({
     user: state.user.data,
     testimonial: state.testimonial.data,
-    following: state.following.data
+    following: state.following.data,
+    favorites: state.favorites.data
 }))(MainPage)
