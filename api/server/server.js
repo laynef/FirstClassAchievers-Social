37aaa0d349cca126d9e9005deddd79373a83bbed
laynefaler <laynefaler@gmail.com>
2017-04-27 16:21:51 -0700

    deploy

--- api/server/server.js
+++ api/server/server.js
@@ -18,7 +18,7 @@ const _ = require('lodash')
 let clients = []
 
 // port settings
-let port = process.env.PORT || 3214
+let port = process.env.PORT
 
 // web socket protocol on localhost on port 3214
 server.listen(port, () => {
