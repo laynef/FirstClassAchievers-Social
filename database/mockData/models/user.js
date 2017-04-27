71946431f58f06c390165f95fc11be0399f8bce7
laynefaler <laynefaler@gmail.com>
2017-04-27 16:34:43 -0700

    ready for dev

--- database/mockData/models/user.js
+++ database/mockData/models/user.js
@@ -1,29 +1,29 @@
-const Profile = require('../../database/models/index').Profile
-const Testimonial = require('../../database/models/index').Testimonial
-const Following = require('../../database/models/index').Following
-const Favorite = require('../../database/models/index').Favorite
-const User = require('../../database/models/index').User
-const Message = require('../../database/models/index').Message
+const Profile = require('../../../database/models/index').Profile
+const Testimonial = require('../../../database/models/index').Testimonial
+const Following = require('../../../database/models/index').Following
+const Favorite = require('../../../database/models/index').Favorite
+const User = require('../../../database/models/index').User
+const Message = require('../../../database/models/index').Message
 const axios = require('axios')
 
 
 axios.post(`http://localhost:3214/auth/local/fixtures`, {
     email: `admin@email.com`,
     password: `pass1234`,
-    firstName: 'Master',
-    lastName: 'Admin',
+    firstName: 'Bionica',
+    lastName: 'Johnson',
     image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493268974/smqbmnavbzfsfssjf1hp.jpg',
     id: 1
 }).then(resp => {
     Testimonial.create({
-        author: 'Master Admin',
+        author: 'Bionica Johnson',
         message: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`,
         user_id: 1,
         image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493268974/smqbmnavbzfsfssjf1hp.jpg',
         likes: 0
     }).then(response => {
         Testimonial.create({
-            author: 'Monica Santa',
+            author: 'Monica Saint',
             message: `"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"`,
             user_id: 2,
             image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493225294/s20botoerwinfvkadqow.jpg',
@@ -47,12 +47,12 @@ axios.post(`http://localhost:3214/auth/local/fixtures`, {
     email: `monica@email.com`,
     password: `pass1234`,
     firstName: 'Monica',
-    lastName: 'Santa',
+    lastName: 'Saint',
     image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493225294/s20botoerwinfvkadqow.jpg',
     id: 2
 }).then(resp => {
     Testimonial.create({
-        author: 'Monica Santa',
+        author: 'Monica Saint',
         message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
         user_id: 2,
         image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493225294/s20botoerwinfvkadqow.jpg',
@@ -60,7 +60,7 @@ axios.post(`http://localhost:3214/auth/local/fixtures`, {
     })
     .then(response => {
         Testimonial.create({
-            author: 'Master Admin',
+            author: 'Bionica Johnson',
             message: `Donec mauris sapien, efficitur vitae tristique et, venenatis id ligula. Quisque ut elit lacus. Nam a tortor at purus placerat aliquet. Nulla facilisi. Proin tristique cursus neque sit amet sollicitudin. Aenean vel augue condimentum diam condimentum condimentum ac vel tellus. Suspendisse varius mattis dictum. Etiam quis justo porttitor, vestibulum mauris at, mollis lorem. Duis ac dui vel urna consequat condimentum in ut enim. Donec sit amet urna id elit tempor posuere vitae a tortor.`,
             user_id: 1,
             image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493268974/smqbmnavbzfsfssjf1hp.jpg',
@@ -96,14 +96,14 @@ axios.post(`http://localhost:3214/auth/local/fixtures`, {
     })
     .then(response => {
         Testimonial.create({
-            author: 'Master Admin',
+            author: 'Bionica Johnson',
             message: `In et ipsum id lorem mattis vehicula. Duis id hendrerit nunc. Donec maximus tempus purus, euismod pellentesque dui vulputate vitae. Donec consequat magna dui, vitae rutrum quam pulvinar vitae. Cras vehicula auctor urna, quis congue ligula posuere in. Nunc tincidunt dui sit amet sollicitudin vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam vel velit facilisis enim tristique facilisis vel eget nibh. Morbi quis feugiat mauris. Suspendisse vitae felis libero. Donec a finibus tortor.`,
             user_id: 1,
             image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493268974/smqbmnavbzfsfssjf1hp.jpg',
             likes: 0
         })
         Testimonial.create({
-            author: 'Monica Santa',
+            author: 'Monica Saint',
             message: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`,
             user_id: 2,
             image: 'http://res.cloudinary.com/dzllxh0km/image/upload/v1493225294/s20botoerwinfvkadqow.jpg',
