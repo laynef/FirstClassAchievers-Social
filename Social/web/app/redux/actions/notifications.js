b25d3c1cf5ce2e3e87abd1adf959ee4a01769f0c
laynefaler <laynefaler@gmail.com>
2017-05-04 10:40:30 -0700

    nification actions

--- Social/web/app/redux/actions/notifications.js
+++ Social/web/app/redux/actions/notifications.js
@@ -1,3 +1,61 @@
 import axios from 'axios'
 import actionTypes from '../store/actionTypes'
 
+
+export function getNotifications(userId) {
+	return function(dispatch) {
+		dispatch({type: actionTypes.GET_NOTIFICATIONS_PENDING})
+		axios.get(`/api/notify/${userId}/`)
+			.then((response) => {
+					dispatch({
+						type: actionTypes.GET_NOTIFICATIONS_SUCCESS,
+						payload: response.data
+					})
+				})
+				.catch((err) => {
+					dispatch({
+						type: actionTypes.GET_NOTIFICATIONS_ERROR,
+						payload: err
+					})
+				})			
+	}
+}
+
+export function createNotifications(data, userId) {
+	return function(dispatch) {
+		dispatch({type: actionTypes.CREATE_NOTIFICATIONS_PENDING})
+		axios.post(`/api/notify/${userId}/`, data)
+			.then((response) => {
+					dispatch({
+						type: actionTypes.CREATE_NOTIFICATIONS_SUCCESS,
+						payload: response.data
+					})
+				})
+				.catch((err) => {
+					dispatch({
+						type: actionTypes.CREATE_NOTIFICATIONS_ERROR,
+						payload: err
+					})
+				})			
+	}
+}
+
+export function setNotifications(data, userId) {
+	return function(dispatch) {
+		dispatch({type: actionTypes.SET_NOTIFICATIONS_PENDING})
+		axios.patch(`/api/notify/${userId}/`, data)
+			.then((response) => {
+					dispatch({
+						type: actionTypes.SET_NOTIFICATIONS_SUCCESS,
+						payload: response.data
+					})
+				})
+				.catch((err) => {
+					dispatch({
+						type: actionTypes.SET_NOTIFICATIONS_ERROR,
+						payload: err
+					})
+				})			
+	}
+}
+
