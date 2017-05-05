4e5e1fbabfe14e3666ff60053912e18778793f78
laynefaler <laynefaler@gmail.com>
2017-05-05 13:32:47 -0700

    setting up routes

--- Ecommerce/api/server/routes/routes.js
+++ Ecommerce/api/server/routes/routes.js
@@ -4,9 +4,62 @@ const controllers = require('../models/models')
 
 // api/ server roots
 
-// user
-router.get('/user', controllers.user.get)
-router.post('/user', controllers.user.post)
+// review
+router.get('/review', controllers.review.get)
+router.post('/review', controllers.review.post)
+router.patch('/review', controllers.review.patch)
+router.delete('/review', controllers.review.delete)
+
+// credit card
+router.get('/credit/card/:all', controllers.creditCard.get)
+
+router.get('/credit/card/:itemId', controllers.creditCard.get)
+router.post('/credit/card/:itemId', controllers.creditCard.post)
+router.patch('/credit/card/:itemId', controllers.creditCard.patch)
+router.delete('/credit/card/:itemId', controllers.creditCard.delete)
+
+// indiviual product
+router.get('/product/:itemId', controllers.product.individual.get)
+router.post('/product/:itemId', controllers.product.individual.post)
+router.patch('/product/:itemId', controllers.product.individual.patch)
+router.delete('/product/:itemId', controllers.product.individual.delete)
+
+// category product
+router.get('/product/:itemId', controllers.product.category.get)
+
+// order
+router.get('/order/:itemId', controllers.order.get)
+router.post('/order/:itemId', controllers.order.post)
+router.patch('/order/:itemId', controllers.order.patch)
+router.delete('/order/:itemId', controllers.order.delete)
+
+// favorites
+router.get('/favorites/:itemId', controllers.favorites.get)
+router.post('/favorites/:itemId', controllers.favorites.post)
+router.patch('/favorites/:itemId', controllers.favorites.patch)
+
+// individual favorites
+router.get('/favorites/:listId', controllers.favorites.individual.get)
+router.post('/favorites/:listId', controllers.favorites.individual.post)
+router.patch('/favorites/:listId', controllers.favorites.individual.patch)
+
+// notify
+router.get('/notify', controllers.notify.get)
+router.post('/notify', controllers.notify.post)
+router.patch('/notify', controllers.notify.patch)
+
+// profile
+router.get('/profile/:itemId', controllers.profile.get)
+router.post('/profile/:itemId', controllers.profile.post)
+router.patch('/profile/:itemId', controllers.profile.patch)
+
+// coupon
+// router.get('/coupon', controllers.coupon.get)
+// router.post('/coupon', controllers.coupon.post)
+// router.patch('/coupon', controllers.coupon.patch)
+
+
+
 
 // export router for server.js
 module.exports = router
