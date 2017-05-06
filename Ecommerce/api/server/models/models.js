62ef2907b76f6e25e138033842c588083763400d
laynefaler <laynefaler@gmail.com>
2017-05-05 17:38:27 -0700

    keys

--- Ecommerce/api/server/models/models.js
+++ Ecommerce/api/server/models/models.js
@@ -9,6 +9,31 @@ const Order = require('../../../database/models/index').Order
 const Favorites = require('../../../database/models/index').Favorites
 const Coupon = require('../../../database/models/index').Coupon
 
+let keys = {
+    categories: {
+        0: 'HAT',
+        1: 'T_SHIRT',
+        2: 'LONG_SLEEVE',
+        3: 'JACKET',
+        4: 'COAT',
+        5: 'DRESS',
+        6: 'PANTS',
+        7: 'SKIRT',
+        8: 'UNDERWEAR',
+        9: 'SOCKS',
+        10: 'TANK_TOP',
+        11: 'BLOUSE',
+        12: 'SHORTS'
+    },
+    age: {
+        0: 'ADULT',
+        1: 'YOUTH'
+    },
+    gender: {
+        0: 'FEMALE',
+        1: 'MALE'
+    }
+}
 
 module.exports = {
     review: {
