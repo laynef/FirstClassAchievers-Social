347dca294b0e7ccbc92efb9e09897b23548775eb
laynefaler <laynefaler@gmail.com>
2017-05-05 17:33:46 -0700

    categories

---
+++ Ecommerce/api/categories.js
@@ -0,0 +1,15 @@
+module.exports = {
+    0: 'HAT',
+    1: 'T_SHIRT',
+    2: 'LONG_SLEEVE',
+    3: 'JACKET',
+    4: 'COAT',
+    5: 'DRESS',
+    6: 'PANTS',
+    7: 'SKIRT',
+    8: 'UNDERWEAR',
+    9: 'SOCKS',
+    10: 'TANK_TOP',
+    11: 'BLOUSE',
+    12: 'SHORTS'
+}
