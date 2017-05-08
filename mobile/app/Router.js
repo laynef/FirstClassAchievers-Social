ab951cc5c7a538541add60d3cf709cb6228b4258
laynefaler <laynefaler@gmail.com>
2017-05-08 10:14:55 -0700

    commons

--- mobile/app/Router.js
+++ mobile/app/Router.js
@@ -3,9 +3,14 @@ import {Actions, Scene, Router} from 'react-native-router-flux'
 
 // Pages
 import MainPage  from './pages/MainPage'
+import AuthPage  from './pages/AuthPage'
 
 const RouterComponent = () => (
     <Router>
+        <Scene key="auth">
+          <Scene key="login" component={AuthPage} title="Please Login" />
+        </Scene>
+
         <Scene key="home" component={MainPage} />
     </Router>
 )
