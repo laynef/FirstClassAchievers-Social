5653a9fbea59464c79c3db7cd892a5c9753a8d9c
laynefaler <laynefaler@gmail.com>
2017-06-25 21:37:52 -0700

    improve hashing function

--- api/server/deviceToken/bcrypt.js
+++ api/server/deviceToken/bcrypt.js
@@ -1,10 +1,11 @@
-// SALT == WEB TOKEN
+// setTimeout for expires
+
 const genSalt = (num) => {
-    if (num < 99999999) return (num * 4396).toString(16) + (num * 4396).toString(8)
+    if (num < 999999) return (num * 4396).toString(16) + (num * 4396).toString(8)
     return num.toString(16) + num.toString(8)
 }
 const createHash = (data, salt, callback) => {
-    let str = JSON.stringify(data + salt).split('').map((e, i) => (e.charCodeAt(0) + (31 * i)).toString(16)).join('') + salt
+    let str = JSON.stringify(data + salt).split('').map((e, i) => (e.charCodeAt(0) + (3 * i)).toString(16)).join('') + JSON.stringify(data + salt).split('').map((e, i) => (e.charCodeAt(0) + (3 * i)).toString(8)).join('')
     callback(`$${str}`)
 }
 const verifyHash = (hashedData, data, salt, callback) => {
@@ -13,9 +14,12 @@ const verifyHash = (hashedData, data, salt, callback) => {
     })
 }
 
+const initialToken = () => Math.floor(Math.random() * 99999999999) + 10000000000
+
 module.exports = {
     genSalt: genSalt,
     createHash: createHash,
-    verifyHash: verifyHash
+    verifyHash: verifyHash,
+    initialToken: initialToken
 }
 
