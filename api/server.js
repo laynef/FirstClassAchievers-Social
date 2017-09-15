810d9e038fc941da441bb6aaa28a34262ee738ef
laynefaler <laynefaler@gmail.com>
2017-09-14 20:33:33 -0700

    deploy

--- api/server.js
+++ api/server.js
@@ -4,18 +4,12 @@ const morgan = require('morgan');
 const cors = require('cors');
 const parser = require('body-parser');
 const router = require('./routes/index');
-// const amqp = require('amqp');
 const sess = require('express-session');
 const RedisStore = require('connect-redis')(sess);
-// const http = require('http');
 let client = require('./caches/redis');
 
 // Express servers
 const app = new Express();
-// const server = http.createServer(app);
-// const io = require('socket.io').listen(server);
-// io.path('/ws');
-// const messager = amqp.createConnection({url: process.env.RABBIT_PORT});
 
 // Middleware Functions
 const shouldCompress = (req, res) => (req.headers['x-no-compression'] ? false : compression.filter(req, res));
