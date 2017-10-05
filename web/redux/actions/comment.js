d062a32018aabfeb0982348ef175d14434fdb5d1
laynefaler <laynefaler@gmail.com>
2017-10-04 23:21:47 -0700

    deploy

--- web/redux/actions/comment.js
+++ web/redux/actions/comment.js
@@ -5,7 +5,7 @@ import actionTypes from '../store/actionTypes';
 export function getComment() {
 	return function(dispatch) {
 		dispatch({type: actionTypes.GET_COMMENT_PENDING});
-		axios.get(`https://still-wildwood-47727.herokuapp.com/api/v1/comments`)
+		axios.get(`${process.env.API_URL}/api/v1/comments`)
 			.then((response) => {
 				dispatch({
 					type: actionTypes.GET_COMMENT_SUCCESS,
@@ -24,7 +24,7 @@ export function getComment() {
 export function setComment(data, id) {
 	return function(dispatch) {
 		dispatch({type: actionTypes.SET_COMMENT_PENDING});
-		axios.post(`https://still-wildwood-47727.herokuapp.com/api/v1/comments/${id}`, data)
+		axios.post(`${process.env.API_URL}/api/v1/comments/${id}`, data)
 			.then((response) => {
 				dispatch({
 					type: actionTypes.SET_COMMENT_SUCCESS,
@@ -43,7 +43,7 @@ export function setComment(data, id) {
 export function likeComment(data, id) {
 	return function(dispatch) {
 		dispatch({type: actionTypes.UPDATE_COMMENT_PENDING});
-		axios.patch(`https://still-wildwood-47727.herokuapp.com/api/v1/like/comment/${id}`, data)
+		axios.patch(`${process.env.API_URL}/api/v1/like/comment/${id}`, data)
 			.then((response) => {
 				dispatch({
 					type: actionTypes.UPDATE_COMMENT_SUCCESS,
--- web/redux/actions/friends.js
+++ web/redux/actions/friends.js
@@ -5,7 +5,7 @@ import actionTypes from '../store/actionTypes';
 export function getFriends(id) {
 	return function(dispatch) {
 		dispatch({type: actionTypes.GET_FRIENDS_PENDING});
-		axios.get(`https://still-wildwood-47727.herokuapp.com/api/v1/friends/${id}`)
+		axios.get(`${process.env.API_URL}/api/v1/friends/${id}`)
 			.then((response) => {
 				dispatch({
 					type: actionTypes.GET_FRIENDS_SUCCESS,
